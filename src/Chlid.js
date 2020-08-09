b7ab5e266a752cbb774b1b09d02b313e32de9f4e
Hassannawazish <50699910+Hassannawazish@users.noreply.github.com>
2020-08-09 22:36:54 +0500

    Updat1

---
+++ src/Chlid.js
@@ -0,0 +1,11 @@
+import React, { useContext } from 'react';
+import ValueContext from './ValueContext';
+function Child() {
+    let value = useContext(ValueContext)
+  return (
+    <div>
+      Child {value}
+    </div>
+  );
+}
+export default Child;
---
+++ src/Parent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Child from './Chlid';
+function Parent(props) {
+  return (
+    <div>
+      Parent
+      <Child></Child>
+    </div>
+  );
+}
+export default Parent;
